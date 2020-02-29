63b4b8b31dfaa902e880bb68fee3ceb9199a58d2
munara90 <58119556+munara90@users.noreply.github.com>
2020-02-29 15:47:00 -0500

    Update CancelReservation.java

--- src/Test/CancelReservation.java
+++ src/Test/CancelReservation.java
@@ -3,5 +3,9 @@ package Test;
 public class CancelReservation {
     public static void main(String[] args) {
         System.out.println("Hey, Batch15");
+        
+        int num=435; 
+        double d=321.54;
+        
     }
 }
