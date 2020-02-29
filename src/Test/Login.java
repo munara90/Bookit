d91cf013cc2bf7fb9491e31531fe4b4b61229d43
munara90 <58119556+munara90@users.noreply.github.com>
2020-02-29 15:54:51 -0500

    Update Login.java

--- src/Test/Login.java
+++ src/Test/Login.java
@@ -3,8 +3,8 @@ package Test;
 public class Login {
     public static void main(String[] args) {
         System.out.println("I love my hubby buddy");
+       String str="Batch15";
+        double price=23.4; //this is from github
         
-        
-        String str="Batch15";
     }
 }
