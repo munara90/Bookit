8d8d562f6d0aaf768402d35c04d92fca0c4aa689
Munargul Shaiyrbekova <munara682@gmail.com>
2020-02-29 15:58:20 -0500

    update2 Login.java

--- src/Test/Login.java
+++ src/Test/Login.java
@@ -3,8 +3,7 @@ package Test;
 public class Login {
     public static void main(String[] args) {
         System.out.println("I love my hubby buddy");
-        
-        
         String str="Batch15";
+        Float fl = 432f;
     }
 }
