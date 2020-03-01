5c27bf76c55135c8ca40fec0e1ef1fbc4c8c662d
munara90 <58119556+munara90@users.noreply.github.com>
2020-03-01 09:30:06 -0500

    Update Country.java

--- src/Test/Country.java
+++ src/Test/Country.java
@@ -4,5 +4,6 @@ public class Country {
     public static void main(String[] args) {
         String myCountry="Kyrgyzstan";
         System.out.println(myCountry);
+        System.out.println("I like my country "+myCountry);
     }
 }
